c87e09c69f874f5bcaa91d95f7fda358a80b4d87
Tavirp <152269967+Tavirp@users.noreply.github.com>
2024-03-07 16:38:10 +0100

    Create Aufgabe1 + TodosMutations

---
+++ unit-testing/Tag3/Aufgabe1.js
@@ -0,0 +1,12 @@
+
+// Unit-Tests werden genutzt, um einzelne Methoden oder Funktionen einer
+// Softwareanwendung zu testen. Sie sind schnell, effizient und helfen,
+// Fehler frühzeitig zu erkennen.
+
+// Integrationstests überprüfen, wie verschiedene Teile einer Anwendung 
+// miteinander interagieren. Sie werden genutzt, um Fehler bei der 
+// Zusammenarbeit zwischen Komponenten zu finden.
+
+// End-to-End-Tests sind quasi das Vollprogramm. Sie simulieren die 
+// Benutzerinteraktion mit der Anwendung und dienen dazu, Probleme zu 
+// finden, die nur im Zusammenspiel aller Komponenten auftreten.
